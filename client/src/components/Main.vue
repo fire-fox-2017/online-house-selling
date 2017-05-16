eb8f945825f6ca947b2d08879d3a6d5a8f46bece
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-16 19:03:43 +0700

    adding axios script, semantic ui, import vuex store in main.js src and adding main vue component to list all houses

--- client/src/components/Main.vue
+++ client/src/components/Main.vue
@@ -1,8 +1,35 @@
 <template>
-<h1>Test</h1>
+  <div id="house-list" class="ui items">
+    <div class="ui header center aligned dividing">
+      <h1 class="head"><i class="fort awesome icon"></i>Most Wanted Houses</h1>
+    </div>
+    <div v-for="house in houses" class="item">
+      <div class="ui medium image">
+        <img :src="house.image" draggable="true" data-bukket-ext-bukket-draggable="true">
+      </div>
+      <div class="content">
+        <a class="header">{{house.title}}</a>
+        <div class="meta">
+          <span>{{house.description}}</span>
+        </div>
+        <div class="description">
+          <p></p>
+        </div>
+        <div class="extra">
+          Rp. {{house.price}}
+        </div>
+        <div class="meta">
+          <div class="ui red button label"><i class="trash icon"></i> Delete</div>
+          <div class="ui blue button label"><i class="edit icon"></i> Edit</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
 
   name: 'Main',
@@ -11,9 +38,31 @@ export default {
     return {
 
     };
+  },
+  methods: {
+    ...mapActions([
+      'getHouses'
+    ])
+  },
+  computed: {
+    ...mapGetters([
+      'houses'
+    ])
+  },
+  mounted() {
+    this.getHouses()
   }
 };
 </script>
 
 <style lang="css" scoped>
+  #house-list {
+      width: 50%;
+      margin: 0 auto;
+      padding-top: 80px;
+      padding-bottom: 50px;
+    }
+  .head {
+    padding-bottom: 10px;
+  }
 </style>
